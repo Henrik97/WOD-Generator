83287c3ba8c2ef80aef0aa4fef51ca5c246c2bf3
Henrik97 <henrikwoodgates97@gmail.com>
2023-07-17 22:17:27 +0200

    icons and workout card

--- src/components/workoutCard.vue
+++ src/components/workoutCard.vue
@@ -6,18 +6,26 @@
     
      height="300px"
      width="300px"
+     rounded="50"
+     
      >
      <v-card-action>
-       <v-btn-toggle v-model="savedWorkout">
-            <v-btn :color="savedWorkout ? 'white' : 'red'" icon="mdi-heart">save</v-btn>
+       <v-btn-toggle v-model="savedWorkout" color="red"    variant="plain" rounded> 
+        <v-btn
+                    class="ms-2"
+                    icon="mdi-heart"
+                    :color="savedWorkout ? 'white' : 'red'"
+                  ></v-btn>
         </v-btn-toggle>
      </v-card-action>
-
+     <v-alert v-if="savedWorkout === 0" type="success" text="Saved"></v-alert>
     </v-card>
+
+    
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import  {WorkoutFormat} from '../models/workoutStructure';
 
 
@@ -29,6 +37,7 @@ defineProps<{
 
 
 const savedWorkout = ref(null)
+ 
 
 </script>
 
